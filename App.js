6fe233c717160fd5f90031d46ea4071b4a88b592
Boris Syrpin <betelgeusexru@gmail.com>
2022-11-26 13:01:53 +0500

    change button component

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Enter your goal here"
-        />
+    <>
+      <StatusBar />
+      <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Enter your goal here"
+          />
+          <TouchableOpacity activeOpacity={0.8} style={styles.addButton}>
+            <Text style={styles.buttonText}>Add goal</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.yourGoals}>List of goals</Text>
+        </View>
       </View>
-      <View>
-        <Text style={styles.yourGoals}>List of goals</Text>
-      </View>
-    </View>
+    </>
   );
 }
 
@@ -23,15 +35,30 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   inputContainer: {
+    display: "flex",
+    alignItems: "center",
     flexDirection: "row",
     justifyContent: "space-between",
+    width: "100%",
+    height: 50,
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: "100%",
-    height: 50,
     paddingHorizontal: 16,
+    flex: 1,
+    height: 50,
+  },
+  addButton: {
+    height: 50,
+    padding: 16,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "orange",
+  },
+  buttonText: {
+    color: "white",
   },
   yourGoals: {
     fontSize: 24,
